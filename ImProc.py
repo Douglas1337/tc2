7e3d96696119e8c73d1a0c9618e674db9434559d
Douglas Alves <alves_543@hotmail.com>
2019-04-07 20:42:58 -0300

    adicionada a parte da primeira técnica utilizada pela autora, não posso afirmar que está correta

--- ImProc.py
+++ ImProc.py
@@ -10,13 +10,9 @@ import numpy as np
 img = cv2.imread('blue.jpg')
 imgH = img.shape[0]
 imgW = img.shape[1]
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-vR=0
-vG=0
-vB=0
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 arrayDeRs=[]
 arrayDeGs=[]
@@ -28,3 +24,18 @@ for x in range (imgH):
         arrayDeRs.append(pxVal[0])
         arrayDeGs.append(pxVal[1])
         arrayDeBs.append(pxVal[2])
+        
+# Primeira Técnica
+vRs = sum(arrayDeRs)
+vGs = sum(arrayDeGs)
+vBs = sum(arrayDeBs)
+
+#potenciação de cada banda
+vRs = vRs**
+vGs = vGs**
+vBs = vBs**
+
+sTot = vRs + vGs + vBs
+
+#Raiz quadrada de tudo
+sTot = (sTot ** 1/2)
